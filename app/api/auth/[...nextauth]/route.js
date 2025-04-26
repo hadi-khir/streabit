0c8738a372d598c656b1d8d904fb64cf072bc1a7
Hadi Khir <khir.hadi@gmail.com>
2025-04-26 17:46:54 -0400

    moving auth options

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,6 @@
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/app/lib/auth";
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }; 
--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,6 +1,15 @@
 import { signIn, signOut } from "next-auth/react";
-
+import GoogleProvider from "next-auth/providers/google";
 export const handleSignIn = () => signIn('google');
 
 export const handleSignOut = () => signOut('google');
 
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+};
